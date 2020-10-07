44c6f838244d4de5f182e14c8a5cd478c17da910
Miki <rethneyk@gmail.com>
2020-10-07 19:33:00 +0200

    fsdfdsfsd

---
+++ MyFirstSPDVProject_ConsoleUI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstSPDVProject_ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SPDVI 2021");
+            Console.ReadKey();
+        }
+    }
+}
